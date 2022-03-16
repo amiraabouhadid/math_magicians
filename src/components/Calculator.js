d4a224841f9e7927b62038fcac9c9d61de03c80e
amira <amiraabouhadid@gmail.com>
2022-03-16 13:55:43 +0200

    use state lift up and props handdown to add calculate functionality and display to calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,23 +1,54 @@
+/* eslint-disable react/no-unused-state */
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
-import { Container, Table, Button } from 'react-bootstrap';
+import { Container, Table } from 'react-bootstrap';
+import calculate from '../logic/calculate';
+import CalcButtons from './CalcButtons';
+import Display from './Display';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
-    };
-    this.add = this.add.bind(this);
-  }
+      total: 0,
+      next: null,
+      operation: null,
 
-  add() {
-    this.setState((state) => ({
-      count: state.count + 1,
-    }));
+    };
   }
 
   render() {
-    const { count } = this.state;
+    const {
+      total, next, operation,
+    } = this.state;
+
+    const displayString = (total, operation, next) => {
+      // 8 diff combos (000 and 010 have the same outcome)
+      // 101 is unncessary
+      // 100
+      if (total && !operation && !next) {
+        return total;
+      }
+      // 001
+      if (!total && !operation && next) {
+        return next;
+      }
+      // 110
+      if (total && operation && !next) {
+        return total + operation;
+      }
+      // 011
+      if (!total && operation && next) {
+        return total + operation + next;
+      }
+
+      // 111
+      if (total && operation && next) {
+        return total + operation + next;
+      }
+      // 000 & 010
+      return '0';
+    };
     const buttons = [['AC', '+/-', '%', '÷'], ['7', '8', '9', 'x'], ['4', '5', '6', '-'], ['1', '2', '3', '+'], ['0', '.', '=']];
     const determineColSpan = (row, ind) => {
       if (row.length < 4 && ind === 0) {
@@ -37,6 +68,10 @@ class Calculator extends React.Component {
       }
       return 'light';
     };
+    const calculateTotal = (obj, buttonName) => {
+      this.setState(calculate(obj, buttonName));
+    };
+
     return (
       <Container fluid>
         <Container className="p-5 my-5">
@@ -45,30 +80,18 @@ class Calculator extends React.Component {
             <tbody>
               <tr>
                 <td colSpan="4" className="bg-secondary p-4 text-end text-white w-100">
-                  {count}
+                  <Display text={displayString(total, operation, next)} />
                 </td>
               </tr>
 
-              {buttons.map((row) => (
-                <tr key={row[0]}>
-
-                  {row.map((butt, ind) => (
-
-                    <td
-                      key={butt}
-                      colSpan={determineColSpan(row, ind)}
-                      className={determineBgColor(row, ind)}
-                    >
-                      <Button variant={determineButtonVar(row, ind)} className="w-100">
-                        {butt}
-                      </Button>
-                    </td>
-
-                  ))}
-                </tr>
-
-              ))}
-
+              <CalcButtons
+                buttons={buttons}
+                calculateTotal={calculateTotal}
+                determineColSpan={determineColSpan}
+                determineBgColor={determineBgColor}
+                determineButtonVar={determineButtonVar}
+                calcObj={this.state}
+              />
             </tbody>
 
           </Table>
