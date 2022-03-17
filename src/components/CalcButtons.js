33c41ad4ef3c00c9ebf821a8a63a6588656f1253
amira <amiraabouhadid@gmail.com>
2022-03-17 15:47:31 +0200

    remove instanceOf and propTypes

--- src/components/CalcButtons.js
+++ src/components/CalcButtons.js
@@ -1,6 +1,7 @@
+/* eslint-disable react/prop-types */
+
 import { Button } from 'react-bootstrap';
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const CalcButtons = (props) => {
   const {
@@ -35,12 +36,5 @@ const CalcButtons = (props) => {
     ))
   );
 };
-CalcButtons.propTypes = {
-  buttons: PropTypes.InstanceOf(Array).isRequired,
-  calculateTotal: PropTypes.func.isRequired,
-  determineColSpan: PropTypes.func.isRequired,
-  determineBgColor: PropTypes.func.isRequired,
-  determineButtonVar: PropTypes.func.isRequired,
-  calcObj: PropTypes.shape({ calcObj: PropTypes.string.isRequired }),
-};
+
 export default CalcButtons;
